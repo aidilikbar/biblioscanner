5ba32d69257f8acdf7dbdf3750034f2a7c8473a7
aidil <aidil.ikbar@kpk.go.id>
2025-04-06 22:26:13 +0200

    modify logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Fontawesome> -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,17 +5,18 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('scan.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('scan') }}">
+                        <i class="fas fa-books text-indigo-600 text-2xl"></i> BiblioScanner
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                <!-- Navigation Links 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('scan.index')" :active="request()->routeIs('scan.index')">
+                    <x-nav-link :href="route('scan')" :active="request()->routeIs('scan')">
                         {{ __('BiblioScanner') }}
                     </x-nav-link>
                 </div>
+                -->
             </div>
 
             <!-- Settings Dropdown -->
